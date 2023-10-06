35d5e1e7aef544b309c18a40d366868caf724a8e
BySwiizen <byswiizen@gmail.com>
2023-10-06 15:14:57 +0200

    Add message to plugin enabled and disabled

--- src/main/java/fr/byswiizen/disabledamage/DisableDamage.java
+++ src/main/java/fr/byswiizen/disabledamage/DisableDamage.java
@@ -10,7 +10,18 @@ public class DisableDamage extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("-----------------------");
+        getLogger().info(this.getName() + " v" + this.getDescription().getVersion());
+        getLogger().info("The plugin is enabled.");
+        getLogger().info("-----------------------");
 
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("------------------------");
+        getLogger().info(this.getName() + " v" + this.getDescription().getVersion());
+        getLogger().info("The plugin is disabled.");
+        getLogger().info("------------------------");
     }
     @EventHandler
     public void onDamage(EntityDamageByEntityEvent event) {
