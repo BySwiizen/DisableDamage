b8b0a36d57d62fc7502650d6efff16db1ed3f423
BySwiizen <byswiizen@gmail.com>
2026-06-17 21:24:37 +0200

    Optimize

--- src/main/java/fr/byswiizen/disabledamage/command/subcommands/ReloadSubCommand.java
+++ src/main/java/fr/byswiizen/disabledamage/command/subcommands/ReloadSubCommand.java
@@ -25,7 +25,6 @@ public class ReloadSubCommand {
             DisableDamage.messagesfile.reload();
             sender.reply(ColorUtil.translate(DisableDamage.messagesfile.getString("prefix") + " " + DisableDamage.messagesfile.getString("reload-success")));
         } catch (Exception error) {
-            sender.reply(ColorUtil.translate("&4Error loading files."));
             error.printStackTrace();
         }
     }
